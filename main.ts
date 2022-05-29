5d1832eb1d40b7ff27feb14a5841f100b77001a6
tommyzhang12345678 <102849446+tommyzhang12345678@users.noreply.github.com>
2022-05-29 12:18:59 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
+})
+function doSomething () {
+    basic.showLeds(`
+        . . # . .
+        . . # . #
+        # # # # .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+}
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.pause(500)
+        basic.showLeds(`
+            . . # . .
+            . . # . #
+            # # # # .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(2000)
+        basic.clearScreen()
+        basic.pause(200)
+        doSomething()
+        basic.pause(200)
+        doSomething()
+        basic.pause(200)
+        doSomething()
+        basic.pause(200)
+        doSomething()
+        basic.pause(200)
+        doSomething()
+    }
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
